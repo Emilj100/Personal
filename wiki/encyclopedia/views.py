2e0289e303d0f85963ef43c1bbd6543cdd1ca755
Emilj100 <emilbondo12@gmail.com>
2025-02-24 12:12:30 +0000

    fremskrit på projekt1

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
+import markdown2
 
 from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entry(request, title):
+    entry = util.get_entry(title)
+    if not entry:
+        return render(request, "encyclopedia/error.html")
+    else: 
+        return render(request, "encyclopedia/entry.html", {
+            "content": markdown2.markdown(entry)
+        })
+
 
