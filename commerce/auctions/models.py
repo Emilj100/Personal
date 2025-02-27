d4e462b13110d1fb4b490e0a1fc1882e41355ba9
Emilj100 <emilbondo12@gmail.com>
2025-02-27 14:17:24 +0000

    fremskridt på projekt 2

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -3,4 +3,28 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    watchlist = models.ManyToManyField("Auction", blank=True, related_name="watchers")
+
+class Auction(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    startbid = models.DecimalField(max_digits=19, decimal_places=2)
+    image = models.ImageField(upload_to='auctions/images', blank=True, null=True)
+    category = models.CharField(max_length=64, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    winner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name="won_auctions")
+    the_creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="auctions")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Bid(models.Model):
+    amount = models.DecimalField(max_digits=19, decimal_places=2)
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="bids")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    comment = models.TextField()
+    commentator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE, related_name="comments")
+    created_at = models.DateTimeField(auto_now_add=True)
